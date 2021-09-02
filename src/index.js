9dca8407d03aaf4cd7dc0bedd97e22af5055e42f
komoskoann\\----------- <komoskoann@gmail.com>
2021-09-03 00:05:07 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,42 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketsMap = {};
+
+  bracketsConfig.forEach(function(config) {
+    let openBracket = config[0];
+    let closeBracket = config[1];
+    bracketsMap[openBracket] = closeBracket;
+  });
+
+  let openBrackets = Object.keys(bracketsMap);
+  let closeBrackets = Object.values(bracketsMap);
+  let stack = [];
+
+  for(let i = 0; i < str.length; i++) {
+    let currentSymbol = str[i];
+    let topElement = stack[stack.length - 1];
+
+    if(openBrackets.includes(currentSymbol)) {
+      if (closeBrackets.includes(currentSymbol)) {
+        if(bracketsMap[topElement] === currentSymbol) {
+          stack.pop();
+        } else {
+          stack.push(currentSymbol);
+        }
+      } else {
+        stack.push(currentSymbol);
+      }
+
+    } else {
+      if(stack.length === 0) {
+        return false;
+      }
+      if(bracketsMap[topElement] === currentSymbol) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  
+  return stack.length === 0;
 }
